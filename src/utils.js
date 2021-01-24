b90d613314f185499766a2aa4feb57738eee31f7
Bera <65181065+MuhammedBeraKoc@users.noreply.github.com>
2021-01-24 17:01:10 +0300

    🪑 HOTFIX: added a polyfill for Object.fromEntries

--- src/utils.js
+++ src/utils.js
@@ -3,12 +3,24 @@ const type = (v) => typeof v;
 const isNil = (v) => is(v, undefined) || is(v, null);
 const identity = (x) => x;
 /**
+ * A polyfill for Object.fromEntries
+ * @param {ArrayLike} keyValuePair An ArrayLike object of key value pairs
+ */
+const fromEntries = (keyValuePair) => {
+    if ((Array.isArray(keyValuePair) || keyValuePair instanceof Map) && !isNil(keyValuePair)) {
+        const result = {}
+        Array.from(keyValuePair).forEach(([key, value]) => result[key] = value)
+        return result
+    }
+    throw new Error(`${keyValuePair} is not iterable`)
+}
+/**
  * Curries the given function
  * @param {Function} f A function with 2nd arity
  */
 const curry = (f) => (x, y) => (isNil(y) ? (y) => f(x, y) : f(x, y));
 const arraysOfKeyValuePairToObject = (keys, values) => {
-    return Object.fromEntries(new Map(keys.map((key, index) => [key, values[index]])));
+    return fromEntries(new Map(keys.map((key, index) => [key, values[index]])));
 };
 
 module.exports = {
